65900af45961c3e674c4cbe21366a0e49e0874bf
James Atkinson <j.Atkinson8@newcastle.ac.uk>
2018-03-05 09:41:47 +0000

    Initial Commit

---
+++ ARGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARGames
+//
+//  Created by james atkinson on 05/03/2018.
+//  Copyright © 2018 james atkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
